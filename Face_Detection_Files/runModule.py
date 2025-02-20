c669e4b26b53740dd59ea0326ef022f01bf1869b
Rohan George John <rohjoh3188@gmail.com>
2025-02-20 17:23:38 +0530

    FACE NORMAL PUSH FINAL

--- Face_Detection_Files/runModule.py
+++ Face_Detection_Files/runModule.py
@@ -1,30 +1,14 @@
-import mediapipe as mp
 import cv2 
 import time
-
+import facetrackingmodule as FDM
 
 capture = cv2.VideoCapture("C:/Users/Rohan/Downloads/face1.mp4")
-
-mpDraw          = mp.solutions.drawing_utils
-mpFaceDetection = mp.solutions.face_detection
-mpFace          = mpFaceDetection.FaceDetection(0.75)
+detector = FDM.FaceDetector()
 pTime = 0
 
 while True:
     success, img = capture.read() 
-    imgRGB       = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)    
-    imgresulting = mpFace.process(imgRGB)
-    
-    if imgresulting.detections:
-        for id,detection in enumerate(imgresulting.detections):
-            #mpDraw.draw_detection(img,detection)
-            BoundBoxMain = detection.location_data.relative_bounding_box
-            ih, iw, ic   = img.shape
-            BoundBox     = int(BoundBoxMain.xmin * iw), int(BoundBoxMain.ymin * ih),\
-                           int(BoundBoxMain.width * iw), int(BoundBoxMain.height * ih)
-            cv2.rectangle(img, BoundBox, (0,0,255), 2 )
-            cv2.putText(img,f"{int(detection.score[0]*100)}",
-                        (BoundBox[0],BoundBox[1]-20),cv2.FONT_HERSHEY_PLAIN,2,(0,0,255),2)
+    img,BBox = detector.findFaces(img)
 
     cTime = time.time()
     fps   = 1/(cTime - pTime)
@@ -32,4 +16,4 @@ while True:
 
     cv2.putText(img,str(int(fps)),(10,70),cv2.FONT_HERSHEY_PLAIN,3,(0,0,255),3)
     cv2.imshow("Image", img)
-    cv2.waitKey(5)
+    cv2.waitKey(1)
